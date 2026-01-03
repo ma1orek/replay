0261cec005a485d1d46e37edd36f3fbd7aee5428
ma1orek <idzikbartosz@gmail.com>
2026-01-03 13:10:05 +0100

    🔧 LinkedIn tracking + scrollbar fix with highest specificity

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,31 @@ export default function RootLayout({
             gtag('config', 'G-QK0TMPEF63');
           `}
         </Script>
+        
+        {/* LinkedIn Insight Tag */}
+        <Script id="linkedin-partner" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "8403626";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `}
+        </Script>
+        <Script id="linkedin-analytics" strategy="afterInteractive">
+          {`
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }} alt="" src="https://px.ads.linkedin.com/collect/?pid=8403626&fmt=gif" />
+        </noscript>
       </head>
       <body className="antialiased">
         <Providers>
